c52d8e390f117ef76c056cde32550bd2c151a620
Flub78 <frederic.peignot@free.fr>
2015-08-18 07:18:45 +0200

    Base of ciauth test

---
+++ application/tests/libraries/Ciauth_library_test.php
@@ -0,0 +1,27 @@
+<?php
+
+class Ciauth_library_test extends TestCase
+{
+    public function setUp()
+    {
+        $this->resetInstance();
+        $this->CI->load->library('ciauth');
+        $this->obj = $this->CI->ciauth;
+    }
+
+    public function test_lib()
+    {
+        $this->assertEquals(false, $this->CI->ciauth->is_logged_in(), "Not logged in");       
+        
+        $this->CI->ciauth->login("flub78", "belobelo", false);
+        
+        $this->assertEquals(true, $this->CI->ciauth->is_logged_in(), "Logged in");
+        
+        $this->CI->ciauth->logout();
+
+        $this->assertEquals(false, $this->CI->ciauth->is_logged_in(), "Not logged in");
+        
+        $name = get_class($this);
+    }
+
+}
