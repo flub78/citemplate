ebef7c4cebd6a3a5d5a5ee73ea058742c31c2047
Flub78 <frederic.peignot@free.fr>
2016-01-07 18:01:10 +0100

    Metadata test

--- application/migrations/20160101000000_test_db.php
+++ application/migrations/20160101000000_test_db.php
@@ -4,17 +4,36 @@ class Migration_test_db extends CI_Migration {
 
 	public function up()
 	{
+		
+		$this->dbforge->drop_table('meta_test1', TRUE);
+		
 	    $fields = array(
+			'id' => array(
+				'type' => 'MEDIUMINT',
+				'constraint' => '8',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'name' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '20',
+			),
+			'description' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '100',
+			),
 	       'expiration_date' => array('type' => 'TIMESTAMP',
 	       'null' => true)
 	    );
-	    $this->dbforge->add_column('users', $fields);
+	    $this->dbforge->add_field('meta_test1', $fields);
+	    $this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('groups');
 
 
 	}
 
 	public function down()
 	{
-		$this->dbforge->drop_column('users', 'expiration_date');
+		$this->dbforge->drop_column('meta_test1', 'expiration_date');
 	}
 }
